3eee8db656beb7280bafc9c3e1a97829addc5558
ahmadashrafgalal <ahmadashrafgalal@gmail.com>
2026-03-23 15:40:07 +0200

    update aqeeda

--- src/app/aqeeda/[id]/page.tsx
+++ src/app/aqeeda/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { ahadith } from "@/data/ahadith";
+import { aqeeda } from "@/data/aqeeda";
 import { notFound } from "next/navigation";
 import Link from "next/link";
 
@@ -6,28 +6,28 @@ interface Props {
   params: { id: string };
 }
 
-export default function HadithDetails({ params }: Props) {
-  const hadith = ahadith.find((h) => h.id === Number(params.id));
+export default function aqDetails({ params }: Props) {
+  const aq = aqeeda.find((a) => a.id === Number(params.id));
 
-  if (!hadith) return notFound();
+  if (!aq) return notFound();
 
   return (
     <div className="min-h-screen bg-gray-100 p-6 md:p-12 flex flex-col items-center">
       {/* عنوان الحديث */}
       <h1 className="text-4xl md:text-5xl font-bold mb-8 text-right text-orange-600 w-full md:w-3/4">
-        س{hadith.id}: {hadith.title}
+        س{aq.id}: {aq.title}
       </h1>
 
       {/* محتوى الحديث */}
       <div className="bg-white w-full md:w-3/4 rounded-3xl shadow-lg border border-gray-200 p-8 flex flex-col gap-6">
         <p className="text-right text-gray-700 leading-relaxed text-lg md:text-xl">
-          {hadith.content}
+          {aq.content}
         </p>
 
         {/* مشغل الصوت */}
 
         <audio
-          src={`/ahadith/${hadith.id}.mp3`}
+          src={`/aqeeda/${aq.id}.mp3`}
           controls
           className="w-full rounded-xl"
         />
